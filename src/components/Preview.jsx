ad5053fbbe89e663b35a47f57aa88fe6ca7d4aa2
Animesh Dung Dung <144546851+bravestone2019@users.noreply.github.com>
2025-06-02 15:59:02 +0530

    adding Preview button functionality

---
+++ src/components/Preview.jsx
@@ -0,0 +1,16 @@
+import './Upload.css'
+import Submit from '../assets/submit.png';
+
+const Preview = () => {
+    return(
+        <>
+        <img
+                src={ Submit }
+                alt='upload'
+                className='icon'
+              />
+        </>
+    );
+};
+
+export default Preview;
